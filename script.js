9325a7025731808cca0e2019a33778ac66ff1fd0
duonguwu-git <duongnguyen0327@gmail.com>
2026-01-14 10:23:43 +0700

    kịch bản

---
+++ script.js
@@ -0,0 +1,16 @@
+const fortunes = [
+    // 100 quẻ vui/ổn
+    '2026 của bạn siêu đỉnh, crush rep story 24/7!',
+    'Năm mới ví dày cộm, shopping thoải mái không lo!',
+    // ... thêm hết 100 câu vui/ổn
+
+    // 50 quẻ bình thường
+    '2026 ổn định, không gì bất ngờ.',
+    'Cuộc sống đều đều, chill hàng ngày.',
+    // ... thêm hết 50 câu bình thường
+
+    // 50 quẻ xui
+    '2026 hơi xui, crush ghost liên tục. Mua nước tại quầy 10/5 để giải nghiệp!',
+    'Tiền bay vèo, ví teo lại. Mua nước tại quầy 10/5 để giải nghiệp!',
+    // ... thêm hết 50 câu xui
+];
